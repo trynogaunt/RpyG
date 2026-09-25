b144e609e4ea769fba0ccaa7fea973a2d237cb59
trynogaunt <toussaint.quentin00@gmail.com>
2026-09-25 13:27:09 +0200

    Feat: mettre à jour l'écran d'exploration pour afficher le nom de la zone et de la pièce

--- core/ui/screens/exploration.py
+++ core/ui/screens/exploration.py
@@ -3,7 +3,13 @@ from textual.widgets import Static, OptionList
 from textual.widgets.option_list import Option
 from textual import on
 from core.game.actions import NewGame, Quit
+from core.views.exploration_view import ExplorationView
 
 class ExplorationScreen(BaseScreen):
     def compose(self):
-        yield Static("Exploration Screen")
+        yield Static("", id="zone_name")
+        yield Static("", id="room_name")
+    
+    def update_view(self, view: ExplorationView):
+        self.query_one(Static, id="zone_name").update(view.zone_name)
+        self.query_one(Static, id="room_name").update(view.room_name)
